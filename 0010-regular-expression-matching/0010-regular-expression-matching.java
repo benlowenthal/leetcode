7fdbd3abdc0690e98e33c593a090260d0262f571
Benjamin Lowenthal <62390840+benlowenthal@users.noreply.github.com>
2022-09-10 15:45:23 +0100

    Time: 101 ms (18.23%), Space: 43.4 MB (22.52%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,10 +1,5 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 class Solution {
     public boolean isMatch(String s, String p) {
-        Pattern pattern = Pattern.compile(p, Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(s);
-        return matcher.matches();
+        return s.matches(p);
     }
 }
