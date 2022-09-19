e16c0cfc184b9c6291ec42532a30c846ce310d29
Benjamin Lowenthal <62390840+benlowenthal@users.noreply.github.com>
2022-09-19 17:16:46 +0100

    Time: 41 ms (5.22%), Space: 55 MB (5.47%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,15 +1,14 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set<String> validPosition = new HashSet<>();
-        Set<String> validBlock = new HashSet<>();
+        Set<String> valid = new HashSet<>();
         
         for (int row = 0; row < 9; row++) {
             for (int col = 0; col < 9; col++) {
                 if (board[row][col] != '.') {
                     //try add to set, returns false if value already there
-                    boolean x = validPosition.add(String.format("%s:%d r", board[row][col], row));
-                    boolean y = validPosition.add(String.format("%s:%d c", board[row][col], col));
-                    boolean z = validBlock.add(String.format("%s:%d,%d", board[row][col], row/3, col/3));
+                    boolean x = valid.add(String.format("%s:%d r", board[row][col], row));
+                    boolean y = valid.add(String.format("%s:%d c", board[row][col], col));
+                    boolean z = valid.add(String.format("%s:%d,%d", board[row][col], row/3, col/3));
                     
                     if (!(x && y && z)) return false;
                 }
