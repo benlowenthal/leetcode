c111f07f9ffca7be68e3bab9b72c2cd5c378c270
Benjamin Lowenthal <62390840+benlowenthal@users.noreply.github.com>
2022-09-21 15:20:06 +0100

    Time: 5 ms (41.55%), Space: 69 MB (12.60%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean canJump(int[] nums) {
         //track maximum position we can reach
-        //since jumps can be any length up to that value
         int max = 0;
         
+        //iterate nums within max reach
         for (int i = 0; i < nums.length - 1 && i <= max; i++)
             if (i + nums[i] > max)
                 max = i + nums[i];
