66b70aac4d132672faa641bb18d5682e21fba75c
Benjamin Lowenthal <62390840+benlowenthal@users.noreply.github.com>
2022-09-21 21:17:26 +0100

    Time: 1 ms (10.38%), Space: 43.6 MB (8.05%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -2,25 +2,24 @@ class Solution {
     public void rotate(int[][] matrix) {
         int rows = matrix.length;
         int cols = matrix[0].length;
+        int temp;
         
         //flip on x=y
         for (int r = 0; r < rows; r++) {
             for (int c = r + 1; c < cols; c++) {
-                swap(matrix, r, c, c, r);
+                temp = matrix[r][c];
+                matrix[r][c] = matrix[c][r];
+                matrix[c][r] = temp;
             }
         }
         
-        //flip on y=size/2
+        // flip on y=size/2
         for (int r = 0; r < rows; r++) {
             for (int c = 0; c < cols / 2; c++) {
-                swap(matrix, r, c, r, cols - c - 1);
+                temp = matrix[r][c];
+                matrix[r][c] = matrix[r][cols - c - 1];
+                matrix[r][cols - c - 1] = temp;
             }
         }
-    }
-    
-    private void swap(int[][] nums, int x1, int x2, int y1, int y2) {
-        int temp = nums[x1][x2];
-        nums[x1][x2] = nums[y1][y2];
-        nums[y1][y2] = temp;
     }
 }
