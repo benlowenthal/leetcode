84f4e6db423d818c52040eca8f32f257b397cb49
Benjamin Lowenthal <62390840+benlowenthal@users.noreply.github.com>
2022-09-21 20:48:51 +0100

    Time: 4 ms (26.29%), Space: 44.8 MB (38.93%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,46 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        
+        for (int n : nums) list.add(n);
+        
+//         int solutions = 1;
+//         for (int x = 2; x <= nums.length; x++) solutions *= x;
+        
+        if (nums.length < 2) return List.of(list);
+        
+        return partialPermute(list);
+    }
+    
+    private List<List<Integer>> partialPermute(List<Integer> nums) {
+        if (nums.size() == 2) {
+            int x = nums.get(0);
+            int y = nums.get(1);
+            
+            List<Integer> xl = new ArrayList<>();
+            xl.add(x); xl.add(y);
+            
+            List<Integer> yl = new ArrayList<>();
+            yl.add(y); yl.add(x);
+            
+            List<List<Integer>> list = new ArrayList<>();
+            list.add(xl); list.add(yl);
+            return list;
+        }
+        
+        List<List<Integer>> perms = new ArrayList<>();
+        
+        for (int i = 0; i < nums.size(); i++) {
+            Integer temp = nums.get(i);
+            nums.remove(i);
+            List<List<Integer>> pp = partialPermute(nums);
+            for (int j = 0; j < pp.size(); j++) {
+                pp.get(j).add(temp);
+                perms.add(pp.get(j));
+            }
+            nums.add(i, temp);
+        }
+        
+        return perms;
+    }
+}
