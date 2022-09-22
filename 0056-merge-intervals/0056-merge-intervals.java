a715ad9ceaf5df9dbb7ba3aeb184fc2e50c4f1e1
Benjamin Lowenthal <62390840+benlowenthal@users.noreply.github.com>
2022-09-22 20:17:43 +0100

    Time: 19 ms (25.07%), Space: 54.9 MB (77.22%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        List<int[]> list = new ArrayList<>();
+        
+        Arrays.sort(intervals, (x,y) -> x[0] - y[0]);
+        
+        int low = -1;
+        int high = -1;
+        
+        for (int[] pair : intervals) {
+            if (pair[0] > high) {
+                if (low >= 0 && high >= 0) list.add(new int[] {low, high});
+                low = pair[0];
+            }
+            
+            high = Math.max(high, pair[1]);
+        }
+        list.add(new int[] {low, high});
+        
+        int[][] out = new int[list.size()][2];
+        for (int i = 0; i < list.size(); i++) out[i] = list.get(i);
+        return out;
+    }
+}
