185890b188051f360273eaa4eaace6119aa2d62c
Benjamin Lowenthal <62390840+benlowenthal@users.noreply.github.com>
2022-09-28 16:00:45 +0100

    Time: 18 ms (13.16%), Space: 44.1 MB (47.01%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String simplifyPath(String path) {
+        String[] dirs = path.split("/+");
+        Stack<String> stk = new Stack<>();
+        
+        for (int i = 0; i < dirs.length; i++) {
+            if (dirs[i].equals("..") && !stk.empty())
+                stk.pop();
+            else if (!dirs[i].equals("..") && !dirs[i].equals(".") && !dirs[i].equals(""))
+                stk.push(dirs[i]);
+        }
+        
+        //join dir array into string
+        StringBuilder sb = new StringBuilder();
+        List<String> strs = new ArrayList<>();
+        
+        while (!stk.empty()) strs.add(stk.pop());
+        for (int x = strs.size() - 1; x >= 0; x--) {
+            sb.append("/");
+            sb.append(strs.get(x));
+        }
+        
+        return (sb.length() == 0) ? "/" : sb.toString();
+    }
+}
