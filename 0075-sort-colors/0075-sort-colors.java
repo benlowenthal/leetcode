fe630fb3a075456292b4cce1afc62dc7b3d14a90
Benjamin Lowenthal <62390840+benlowenthal@users.noreply.github.com>
2022-09-28 16:19:34 +0100

    Time: 0 ms (100.00%), Space: 43 MB (12.98%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int r = 0, w = 0;
+        
+        for (int n : nums) {
+            if (n == 0) r++;
+            else if (n == 1) w++;
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (i < r) nums[i] = 0;
+            else if (i < r + w) nums[i] = 1;
+            else nums[i] = 2;
+        }
+    }
+}
