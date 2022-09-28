b742ff8b9e44a5ea17a7ea6bcea139bf2aa58be7
Benjamin Lowenthal <62390840+benlowenthal@users.noreply.github.com>
2022-09-28 16:20:12 +0100

    Time: 1 ms (48.41%), Space: 42.3 MB (64.46%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -2,11 +2,13 @@ class Solution {
     public void sortColors(int[] nums) {
         int r = 0, w = 0;
         
+        //count amount of red and white
         for (int n : nums) {
             if (n == 0) r++;
             else if (n == 1) w++;
         }
         
+        //replace values if in range
         for (int i = 0; i < nums.length; i++) {
             if (i < r) nums[i] = 0;
             else if (i < r + w) nums[i] = 1;
