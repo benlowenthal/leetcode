f38a873b661569f61369fb99576fb5520804c29c
Benjamin Lowenthal <62390840+benlowenthal@users.noreply.github.com>
2022-09-30 20:18:07 +0100

    Time: 2 ms (53.24%), Space: 54.5 MB (13.76%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        List<Integer> idx = new ArrayList<>();
+        
+        int m = matrix[0].length;
+        for (int i = 0; i < matrix.length * m; i++)
+            if (matrix[i/m][i%m] == 0)
+                idx.add(i);
+        
+        for (int id : idx)
+            setZeros(matrix, id/m, id%m);
+    }
+    
+    private void setZeros(int[][] matrix, int i, int j) {
+        for (int x = 0; x < matrix.length; x++) matrix[x][j] = 0;
+        for (int y = 0; y < matrix[0].length; y++) matrix[i][y] = 0;
+    }
+}
