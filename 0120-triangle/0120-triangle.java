6699614209e20a806070c4f6fc2bc479b7b95db9
Benjamin Lowenthal <62390840+benlowenthal@users.noreply.github.com>
2022-10-08 12:54:13 +0100

    Time: 9 ms (34.94%), Space: 43.6 MB (77.87%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -2,6 +2,7 @@ class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         List<Integer> list = triangle.get(triangle.size() - 1);
         
+        //iterate up the triangle adding the min value from below
         for (int i = triangle.size() - 2; i >= 0; i--) {
             List<Integer> row = triangle.get(i);
             
