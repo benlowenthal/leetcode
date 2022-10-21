5abce86edecb9e86bc4ee98de1321504573b42e0
Benjamin Lowenthal <62390840+benlowenthal@users.noreply.github.com>
2022-10-21 19:22:48 +0100

    Time: 34 ms (14.79%), Space: 54.9 MB (81.55%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,7 +3,6 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         
         for (int n : nums) {
-            //check if n was already in map without iteration
             Integer old = map.put(n, 1);
             if (old != null) {
                 if (old+1 > nums.length / 2)
